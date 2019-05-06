394e535ff0d3ccc69aa35f8aae4cb38143741202
teceler <19716939+teceler@users.noreply.github.com>
2019-05-06 18:25:51 -0500

    Alter migration to editor_mode to ReplyDrafts

--- db/migrate/20190505014536_add_editor_mode_to_replies.rb
+++ db/migrate/20190505014536_add_editor_mode_to_replies.rb
@@ -1,8 +1,10 @@
 class AddEditorModeToReplies < ActiveRecord::Migration[5.2]
   def up
     add_column :replies, :editor_mode, :string
+    add_column :reply_drafts, :editor_mode, :string
 
-    Reply.all.each do |reply|
+    replies = Reply.all + ReplyDraft.all
+    replies.each do |reply|
       if reply.content[/<p( [^>]*)?>/] || reply.content[/<br *\/?>/]
         reply.update_columns(editor_mode: 'rtf')
       else
@@ -13,5 +15,6 @@ class AddEditorModeToReplies < ActiveRecord::Migration[5.2]
 
   def down
     remove_column :replies, :editor_mode, :string
+    remove_column :reply_drafts, :editor_mode, :string
   end
 end

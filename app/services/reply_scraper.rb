7977391e24ce970572109a2c6d7217c7152ce704
teceler <19716939+teceler@users.noreply.github.com>
2019-11-12 22:54:57 -0600

    Split into ReplyScraper

---
+++ app/services/reply_scraper.rb
@@ -0,0 +1,120 @@
+class ReplyScraper < Generic::Service
+  def initialize(reply)
+    @reply = reply
+    super()
+  end
+
+  def import
+    @reply.created_at = @reply.updated_at = created_at
+    @reply.content = strip_content(content)
+    @reply.user = set_from_username(@reply, username)
+    @reply.icon = set_from_icon(@reply, img_url, img_keyword) if img_url
+    post_setup if @reply.is_a? Post
+    Audited.audit_class.as_user(@reply.user) do
+      @reply.save!
+    end
+  end
+
+  private
+
+  def post_setup
+    @reply.last_user_id = @reply.user_id
+    @reply.edited_at = @reply.created_at
+  end
+
+  def set_from_username(tag, username)
+    return User.find_by(username: BASE_ACCOUNTS[username]) if BASE_ACCOUNTS.key?(username)
+
+    unless (character = Character.find_by(screenname: [username.tr("-", "_"), username.tr("_", "-")]))
+      user = prompt_for_user(username)
+      character = Character.create!(user: user, name: username, screenname: username)
+      gallery = Gallery.create!(user: user, name: username)
+      CharactersGallery.create!(character_id: character.id, gallery_id: gallery.id)
+    end
+
+    tag.character = character
+    character.user
+  end
+
+  def prompt_for_user(username)
+    if @console_import
+      print("User ID or username for #{username}? ")
+      input = STDIN.gets.chomp
+      return User.find_by(id: input) if input.to_s == input.to_i.to_s
+      User.find_by('lower(username) = ?', input.downcase)
+    else
+      @errors.add(:base, "Unrecognized username: #{username}")
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  def set_from_icon(tag, url, keyword)
+    uri = URI(url)
+    host = uri.host || 'v.dreamwidth.org'
+    url = URI::HTTPS.build(host: host, path: uri.path, fragment: uri.fragment, query: uri.query).to_s
+    icon = Icon.find_by(url: url)
+    return icon if icon
+
+    keyword = parse_keyword(keyword)
+
+    if tag.character
+      icon = tag.character.icons.find_by(keyword: keyword)
+      icon ||= clean_keyword(tag, keyword)
+      return icon if icon
+    end
+
+    create_icon(tag, url, keyword)
+  end
+
+  def parse_keyword(keyword)
+    end_index = keyword.index("(Default)").to_i - 1
+    start_index = (keyword.index(':') || -1) + 1
+    parsed_keyword = keyword[start_index..end_index].strip
+    parsed_keyword = 'Default' if parsed_keyword.blank? && keyword.include?("(Default)")
+    parsed_keyword
+  end
+
+  def clean_keyword(tag, keyword)
+    # split out the last " (...)" from the keyword (which should be at the
+    # very end), if applicable, for without_desc
+    without_desc = nil
+    if keyword.end_with?(')')
+      lbracket = keyword.rindex(' (')
+      if lbracket && lbracket > 0 # without_desc must be non-empty
+        without_desc = keyword[0...lbracket]
+        icon = tag.character.icons.find_by(keyword: without_desc)
+      end
+    end
+    icon ||= kappa_keyword(tag, keyword, without_desc)
+    icon
+  end
+
+  def kappa_keyword(tag, keyword, without_desc)
+    # kappa icon handling - icons are prefixed
+    if tag.user_id == 3 && (spaceindex = keyword.index(" "))
+      unprefixed = keyword[spaceindex..-1]
+      icon = tag.character.icons.detect { |i| i.keyword.ends_with?(unprefixed) }
+      icon ||= tag.character.icons.detect { |i| i.keyword.ends_with?(without_desc[spaceindex..-1]) } if without_desc
+    end
+    icon
+  end
+
+  def create_icon(tag, https_url, keyword)
+    icon = Icon.create!(user: tag.user, url: https_url, keyword: keyword)
+    return icon unless tag.character
+
+    gallery = tag.character.galleries.first
+    if gallery.nil?
+      gallery = Gallery.create!(user: tag.user, name: tag.character.name)
+      CharactersGallery.create!(character_id: tag.character.id, gallery_id: gallery.id)
+    end
+    gallery.icons << icon
+    icon
+  end
+
+  def strip_content(content)
+    return content unless content.ends_with?("</div>")
+    index = content.index('edittime')
+    content[0..index-13]
+  end
+end

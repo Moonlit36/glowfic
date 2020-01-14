cafb1f8e499ec4673d291a1d20a5794e7d1771e5
Edward Jones <throne3d@gmail.com>
2020-01-14 01:51:44 -0300

    Add minio to replicate S3 bucket functionality locally

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,27 @@
-Aws.config.update({
+access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID', 'minioadmin')
+secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY', 'minioadmin')
+bucket_name = ENV.fetch('S3_BUCKET_NAME', 'glowfic-dev')
+config = {
   region: 'us-east-1',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-})
-S3_BUCKET = (Aws::S3::Resource.new.bucket(ENV['S3_BUCKET_NAME']) if ENV['S3_BUCKET_NAME'])
+  credentials: Aws::Credentials.new(access_key_id, secret_access_key),
+}
+
+if ENV.key?('MINIO_ENDPOINT')
+  config[:endpoint] = ENV['MINIO_ENDPOINT']
+  config[:force_path_style] = true
+  Aws.config.update(config)
+
+  client = Aws::S3::Client.new
+  begin
+    client.head_bucket(bucket: bucket_name)
+  rescue StandardError => e
+    puts "creating bucket #{bucket_name}..."
+    client.create_bucket(acl: 'public-read', bucket: bucket_name)
+  end
+else
+  Aws.config.update(config)
+end
+
+S3_BUCKET = Aws::S3::Resource.new.bucket(bucket_name)
 
 Aws::Rails.add_action_mailer_delivery_method(:aws_ses)

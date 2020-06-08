29d4df22bac1030867cb253bede001f9488ebe06
teceler <19716939+teceler@users.noreply.github.com>
2020-06-08 14:10:11 -0500

    fix settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,8 +1,8 @@
 class Setting < Tag
   include Tag::Taggable::Setting
 
-  has_many :parent_setting_tags, class_name: 'TagTag', foreign_key: :tag_id, inverse_of: :child_setting, dependent: :destroy
-  has_many :child_setting_tags, class_name: 'TagTag', foreign_key: :tagged_id, inverse_of: :parent_setting, dependent: :destroy
+  has_many :parent_setting_tags, class_name: 'TagTag', foreign_key: :tag_id, inverse_of: :parent_setting, dependent: :destroy
+  has_many :child_setting_tags, class_name: 'TagTag', foreign_key: :tagged_id, inverse_of: :child_setting, dependent: :destroy
 
   has_many :parent_settings, -> { ordered_by_tag_tag }, class_name: 'Setting', through: :child_setting_tags,
     source: :parent_setting, dependent: :destroy
@@ -11,11 +11,11 @@ class Setting < Tag
   private
 
   def load_setting_tags
-    @setting_list = Tag::List.new(parent_settings.map(&:name))
+    @setting_list = Tag::List.new(parent_settings.pluck(:name))
   end
 
   def save_setting_tags
     return unless setting_list_changed?
-    save_tags(::Setting, new_list: @setting_list, old_list: setting_list_was, assoc: parent_settings)
+    save_tags(::Setting, new_list: @setting_list, old_list: setting_list_was, assoc: parent_settings, join: child_setting_tags)
   end
 end

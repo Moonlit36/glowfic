1e2bca39f806d88af1ada09089564edf8d88ebbe
teceler <19716939+teceler@users.noreply.github.com>
2020-08-02 19:07:10 -0500

    Restore cache invalidation (probably a merge error)

--- app/models/post.rb
+++ app/models/post.rb
@@ -47,6 +47,7 @@ class Post < ApplicationRecord
   before_update :set_timestamps
   before_validation :set_last_user, on: :create
   after_commit :notify_followers, on: :create
+  after_commit :invalidate_caches, on: :update
 
   NON_EDITED_ATTRS = %w(id created_at updated_at edited_at tagged_at last_user_id last_reply_id section_order)
   NON_TAGGED_ATTRS = %w(icon_id character_alias_id character_id)
